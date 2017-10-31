4148da9e0c95ffb36f063443432e1dba5d9831bd
mafd16 <mngfagerlund@gmail.com>
2017-10-31 12:22:10 +0100

    For kmom01

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ramverk2-me
+
+
+[![Build Status](https://travis-ci.org/mafd16/ramverk2-me.svg)](https://travis-ci.org/mafd16/ramverk2-me)
+[![BCH compliance](https://bettercodehub.com/edge/badge/mafd16/ramverk2-me?branch=master)](https://bettercodehub.com/)
+
+Me-page for the course Ramverk2 at Blekinge Institute of Technology
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var height = window.innerHeight;
+//var height = window.innerHeight;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+//router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('index', { title: 'Martins Me-sida' });
 });
 
 /* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Om Ramverk2' });
+//router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res) {
+    res.render('about', { title: 'Om Ramverk2' });
 });
 
 /* GET report page. */
-router.get('/report', function(req, res, next) {
-  res.render('report', { title: 'Rapporter' });
+//router.get('/report', function(req, res, next) {
+router.get('/report', function(req, res) {
+    res.render('report', { title: 'Rapporter' });
 });
 
 module.exports = router;
