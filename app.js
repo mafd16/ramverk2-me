58f7d51806d260273b9912a35466d82beea2704c
mafd16 <mngfagerlund@gmail.com>
2017-10-31 16:05:04 +0100

    kmom01 ready

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -15,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,7 +32,6 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-//app.use(function(err, req, res, next) {
 app.use(function(err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message;
