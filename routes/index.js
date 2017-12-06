479b08ff58847d94747d504638a55099329cf53b
Martin Fagerlund <mngfagerlund@gmail.com>
2017-12-06 20:14:15 +0000

    just for saving

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
+// MongoDB
+//const mongodb = require('../src/database/mongodb.js');
+
+// The routes //
+
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index', { title: 'Martins Me-sida' });
@@ -32,8 +38,40 @@ router.get('/chatt', function(req, res) {
 });
 
 /* GET database page. */
-router.get('/crud', function(req, res) {
-    res.render('crud', { title: 'Databas' });
+router.get('/crud', async (req, res) => {
+    // MongoDB
+    var mongo = require("mongodb").MongoClient;
+    // The dsn
+    dsn =  process.env.DBWEBB_DSN || "mongodb://mongodb_redovisa:27017/vehicles";
+
+    try {
+        const db  = await mongo.connect(dsn);
+        const col = await db.collection("bikes");
+        //await col.insert({brand: "Merida", gears: "Shimano", type: "mtb" });
+        const result = await col.find().toArray();
+        await db.close();
+        //res.json(result);
+        result2 = [];
+        for (var i = 0; i < result.length; i++) {
+            var k = 0;
+            for (var j = 0; j < result[i].length; j++) {
+                if (k >= 1) {
+                    result2.push(result[i][j]);
+                }
+
+            }
+
+        }
+        console.log("Result: " + result);
+        //res.json(result);
+        result2 = JSON.stringify(result);
+        res.render('crud', { title: 'Databas', message: result });
+    } catch (err) {
+        console.log(err);
+        //response.json(err);
+        res.render('crud', { title: 'Databas', data: err });
+    }
 });
+
 
 module.exports = router;
--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,53 +1,31 @@
 /**
- * Connect to the database and perform CRUD.
+ * Database connection and crud functions
+ *
  */
-"use strict";
+//"use strict";
 
+/*
 // MongoDB
-const mongo = require("mongodb").MongoClient;
-const dsn =  process.env.DBWEBB_DSN || "mongodb://localhost:27017/bikes";
+var mongo = require("mongodb").MongoClient
+, assert = require("assert");
+
+// The dsn
+dsn =  process.env.DBWEBB_DSN || "mongodb://localhost:27017/vehicles";
+
+var mongodb = {
 
 
 
-// Return a JSON object with list of all documents within the collection.
-app.get("/list", async (request, response) => {
-    try {
-        let res = await findInCollection(dsn, "crowd", {}, {}, 0);
 
-        console.log(res);
-        response.json(res);
-    } catch (err) {
-        console.log(err);
-        response.json(err);
+    async function showAll() {
+        const db  = await mongo.connect(dsn);
+        const col = await db.collection("bikes");
+        await col.insert({brand: "Merida", gears: "Shimano", type: "mtb" });
+        const res = await col.find(criteria, projection).limit(limit).toArray();
+        await db.close();
+        return res;
     }
-});
 
-
-
-/**
- * Find documents in an collection by matching search criteria.
- *
- * @async
- *
- * @param {string} dsn        DSN to connect to database.
- * @param {string} colName    Name of collection.
- * @param {object} criteria   Search criteria.
- * @param {object} projection What to project in results.
- * @param {number} limit      Limit the number of documents to retrieve.
- *
- * @throws Error when database operation fails.
- *
- * @return {Promise<array>} The resultset as an array.
- */
-async function findInCollection(dsn, colName, criteria, projection, limit) {
-    const db  = await mongo.connect(dsn);
-    const col = await db.collection(colName);
-    const res = await col.find(criteria, projection).limit(limit).toArray();
-
-    await db.close();
-
-    return res;
 }
 
-
-module.exports = mongo;
+module.exports = mongodb;
